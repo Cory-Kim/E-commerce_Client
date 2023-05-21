585c57b5e8a9a9d8f4b83ee833a00b4e46bdaf42
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-05-20 20:58:52 -0700

    worked little bit on header page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+    return (
+        <>
+            <header className="header-top-strip py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-6">
+                            <p className="text-white mb-0">
+                                Free Shipping over $100 & Free Returns
+                            </p>
+                        </div>
+                        <div className="col-6">
+                            <p className="text-end text-white mb-0">
+                                Hotline:{" "}
+                                <a className="text-white" href="tel:+1 7824132">
+                                    +1 7786321432
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
-export default Header
+            <header className="header-upper py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-2">
+                            <h2>
+                                <Link>CyberMarket</Link>
+                            </h2>
+                        </div>
+                        <div className="col-5"></div>
+                        <div className="col-5"></div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
