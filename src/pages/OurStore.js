439707b5951a0992d8b533cfd5e73b6ad5cd5db0
LAPTOP-23C0I0R6\cdoky <cdokyung@gmail.com>
2023-05-26 01:05:19 -0700

    finished comparing products page.

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -4,6 +4,7 @@ import BreadCrumb from "../components/BreadCrumb";
 import { Helmet } from "react-helmet";
 import Meta from "../components/Meta";
 import ProductCard from "../components/ProductCard";
+import Color from "../components/Color";
 
 const OurStore = () => {
     const [grid, setGrid] = useState(4);
@@ -93,26 +94,7 @@ const OurStore = () => {
                                     <h5 className="sub-title">Colors</h5>
                                     {/* <div><Color /></div> */}
                                     <div>
-                                        <ul className="colors ps-0">
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                        </ul>
+                                        <Color />
                                     </div>
                                     <h5 className="sub-title">Size</h5>
                                     <div>
